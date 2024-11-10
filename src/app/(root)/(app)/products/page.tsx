6ffa78f965971ba58a9d14710a314339df0d5c77
jeonilshin <jeonilshinbusiness@gmail.com>
2024-11-10 21:03:05 +0900

    fixed alot

--- src/app/(root)/(app)/products/page.tsx
+++ src/app/(root)/(app)/products/page.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from 'next'
-import { getBlogPosts } from '@/utils/get-blog-posts'
 import PageTitle from '../components/PageTitle'
-import BlogPostItem from './components/BlogPostItem'
 
 export const metadata: Metadata = {
   title: 'Amixtra | Blog',
@@ -15,24 +13,11 @@ export const metadata: Metadata = {
 }
 
 export default async function BlogPage() {
-  const blogs = getBlogPosts().sort((a, b) => {
-    if (new Date(a.metadata.publishedAt) > new Date(b.metadata.publishedAt)) {
-      return -1
-    }
-    return 1
-  })
 
   return (
     <>
-      <PageTitle title="Blogs" />
-      <h1 className='text-6xl mb-6'>CHAT</h1>
-      <ul className="grid grid-cols-1 gap-3 md:mb-5">
-        {blogs.map((post) => (
-          <li key={post.slug}>
-            <BlogPostItem post={post} />
-          </li>
-        ))}
-      </ul>
+      <PageTitle title="Products" />
+      <h1 className='text-6xl mb-6'>Updating Soon...</h1>
     </>
   )
 }
--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -1,7 +1,12 @@
+import Image from "next/image";
+
 const Logo: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 137.41 137.41" height={16} width={16} {...props}>
-    <path d="M68.71 0a68.71 68.71 0 1068.7 68.71A68.71 68.71 0 0068.71 0zm26.21 82.92a12 12 0 01-12 12H54.49a12 12 0 01-12-12V54.49a12 12 0 0112-12h28.43a12 12 0 0112 12z"></path>
-  </svg>
+  <Image
+    src="/readme/logo-big.png"
+    alt="Amixtra Logo"
+    width="40"
+    height="40"
+  />
 )
 
 export default Logo
